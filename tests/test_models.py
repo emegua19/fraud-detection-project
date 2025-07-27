8a741c1cbb92cd3ee473f815f8484f6f13494387
yitbarek <ebnenode@gmail.com>
2025-07-27 20:46:10 +0000

    update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,25 +31,3 @@ def test_lightgbm_fraud():
     preds = model.predict(X_test)
     score = f1_score(y_test, preds)
     assert score > 0.80, f"Low F1 score for LightGBM on fraud data: {score:.4f}"
-
-# -----------------------------
-# Logistic Regression - Creditcard (SMOTE)
-# -----------------------------
-def test_logistic_regression_credit():
-    X_train, X_test, y_train, y_test = load_data("data/processed/creditcard_balanced_smote.csv", "Class")
-    model = LogisticRegression(max_iter=3000, solver="saga", class_weight="balanced", random_state=42)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-    score = f1_score(y_test, preds)
-    assert score > 0.65, f"Low F1 score for logistic regression on creditcard data: {score:.4f}"
-
-# -----------------------------
-# LightGBM - Creditcard (SMOTE)
-# -----------------------------
-def test_lightgbm_credit():
-    X_train, X_test, y_train, y_test = load_data("data/processed/creditcard_balanced_smote.csv", "Class")
-    model = LGBMClassifier(random_state=42)
-    model.fit(X_train, y_train)
-    preds = model.predict(X_test)
-    score = f1_score(y_test, preds)
-    assert score > 0.75, f"Low F1 score for LightGBM on creditcard data: {score:.4f}"
